be2e103c2554b2ff20aaa1813b515ae8e689cbd0
Mac Mbizo <macmbizo@gmail.com>
2021-10-31 23:54:56 +0200

    fixed runtime errors and spellings in variables

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import re
 import long_responses as long
 
 # Function that calculates the probability of the right message
-def message_probability(user_message, recognised_words, single_response=False, require_words=[]):
+def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
     message_certainty = 0
     has_required_words = True
 
@@ -15,7 +15,7 @@ def message_probability(user_message, recognised_words, single_response=False, r
     percentage = float(message_certainty) / float(len(recognised_words))
 
     # Checks that the required words are in the string
-    for word in require_words:
+    for word in required_words:
         if word not in user_message:
             has_required_words = False
             break
@@ -32,7 +32,7 @@ def check_all_messages(message):
 
     def response(bot_response, list_of_words, single_response=False, required_words=[]):
         nonlocal highest_prob_list
-        highest_prob_list[bot_response] = message_probability(message, list_of_words,single_response, require_words)
+        highest_prob_list[bot_response] = message_probability(message, list_of_words,single_response, required_words)
 
     # Responses---------------------------------------------------------------------
     response('Hello!', ['hello', 'hi', 'sup', 'hey', 'heyo'], single_response = True)
