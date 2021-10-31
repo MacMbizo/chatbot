65d6050cd1d777fedffd0b767ecea62cc06d121c
Mac Mbizo <macmbizo@gmail.com>
2021-10-31 23:22:19 +0200

    added function that checks required words in the string

--- main.py
+++ main.py
@@ -6,11 +6,19 @@ def message_probability(user_message, recognised_words, single_response=False, r
     message_certainty = 0
     has_required_words = True
 
+    # Counts how many words are present in each predifined message
     for word in user_message:
         if word in recognised_words:
             message_certainty += 1
 
+    # Calculates the percentage of recognised words in a user message
     percentage = float(message_certainty) / float(len(recognised_words))
+
+    # Checks that the required words are in the string
+    for word in require_words:
+        if word not in user_message:
+            has_required_words = False
+            break
 
 def get_response(user_input):
     split_message = re.split(r'\s+|{,;?!.-]\s*', user_input.lower())
