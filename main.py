2319880cb529880284558e545bd41cb9a408bacb
Mac Mbizo <macmbizo@gmail.com>
2021-11-01 21:30:02 +0200

    updated responses

--- main.py
+++ main.py
@@ -39,6 +39,9 @@ def check_all_messages(message):
     response('I\'m doing fine, and you?', ['how','are', 'you', 'doing'], required_words=['how'])
     response('Thank you!', ['I', 'love', 'code', 'palace'], required_words=['code', 'palace'])
 
+    response(long.R_EATING, ['what', 'you', 'eat'], required_words=['you', 'eat'])
+    
+
     best_match = max(highest_prob_list, key=highest_prob_list.get)
     #print(highest_prob_list)
 
